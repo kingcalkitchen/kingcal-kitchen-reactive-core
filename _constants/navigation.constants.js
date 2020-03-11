3a28c1f61311618e57df066d4a5b82ad83ad0c22
Richard K Heaton <richard.k.heaton@gmail.com>
2020-03-11 14:14:22 -0400

    This commit will introduce core-module to app at current development state.

---
+++ _constants/navigation.constants.js
@@ -0,0 +1,19 @@
+export const navigationConstants = {
+    SIGN_IN: 'SignIn',
+    SIGN_UP: 'SignUp',
+  
+    HOME: 'Home',
+  
+    LOCATIONS: 'Locations',
+    KINGCAL_KITCHEN: 'KingCalKitchen',
+    CUTTING_EDGE_NUTRITION: 'CuttingEdgeNutrition',
+    FGN_FITNESS: 'FgnFitness',
+  
+    MEAM_MENU_HOME: 'MealMenuHome',
+    CATEGORIES: 'Categories',
+    INGREDIENT: 'Ingredient',
+    INGREDIENTS: 'Ingredients',
+    RECIPE: 'Recipe',
+    RECIPESLIST: 'RecipesList',
+    SEARCH: 'Search',
+}
